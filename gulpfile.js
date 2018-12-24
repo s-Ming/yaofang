d5116d300ee695c92217a2cac17b91a6af99728e
s-Ming <840061073@qq.com>
2018-12-24 20:59:16 +0800

    头部，导航

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,12 @@ var sass = require("gulp-sass");
 // 2.书写gulp任务 task(任务名,函数)
 gulp.task("compileSass",function(){
     //(1) 通过路径，拿到文件流 src(路径)
-    gulp.src(["./src/sass/**/*.scss"])
+    return gulp.src(["./src/sass/*.scss"])
     //（2）运输文件流 pipe()
     // (3) 运输过程中，对scss文件编译成css文件 sass()
     //      * 报错不断开 on('error', sass.logError)
     // (4) 将css文件运输出去  dest(文件夹路径)
     .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-
     .pipe(gulp.dest("./src/css"));
 })
 
@@ -21,7 +20,7 @@ gulp.task("compileSass",function(){
 // 监听方法 watch("路径",[任务名])
 
 gulp.task("jt",function(){
-    gulp.watch("./src/sass/**/*.scss",["compileSass"]);
+    gulp.watch("./src/sass/*.scss",gulp.series("compileSass"));
 })
 
 
@@ -86,6 +85,6 @@ gulp.task('server',()=>{
         // 监听文件修改，自动刷新页面
         files:['./src/**/*.html','./src/css/*.css','./src/api/*.php']
     });
-    gulp.watch("./src/sass/**/*.scss",["compileSass"]);
+    gulp.watch("./src/sass/*.scss",gulp.series("compileSass"));
 
 })
